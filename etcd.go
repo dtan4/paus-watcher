35b20b7dfcdd4a66cf7e70c4db29bbadd7da0afb
Daisuke Fujita <dtanshi45@gmail.com>
2016-06-22 20:41:00 +0900

    Watch etcd actions

---
+++ etcd.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"github.com/coreos/etcd/client"
+	"github.com/pkg/errors"
+	"golang.org/x/net/context"
+)
+
+type Etcd struct {
+	keysAPI client.KeysAPI
+}
+
+type EtcdWatchFunc func(*client.Response)
+
+func NewEtcd(endpoint string) (*Etcd, error) {
+	config := client.Config{
+		Endpoints: []string{endpoint},
+		Transport: client.DefaultTransport,
+	}
+
+	c, err := client.New(config)
+
+	if err != nil {
+		return nil, errors.Wrap(err, "Failed to create etcd client.")
+	}
+
+	keysAPI := client.NewKeysAPI(c)
+
+	return &Etcd{keysAPI}, nil
+}
+
+func (c *Etcd) Watch(key string, callback EtcdWatchFunc) error {
+	w := c.keysAPI.Watcher(key, &client.WatcherOptions{Recursive: true})
+
+	for {
+		resp, err := w.Next(context.TODO())
+
+		if err != nil {
+			return errors.Wrap(err, "Failed to iterate.")
+		}
+
+		if resp.Node.Dir {
+			continue
+		}
+
+		callback(resp)
+	}
+}
--- main.go
+++ main.go
@@ -2,8 +2,36 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/signal"
+
+	"github.com/coreos/etcd/client"
 )
 
+func callback(resp *client.Response) {
+	fmt.Printf("[%s] %s\n", resp.Action, resp.Node.Key)
+}
+
 func main() {
-	fmt.Println("paus-watcher")
+	if len(os.Args) != 2 {
+		os.Exit(1)
+	}
+
+	endpoint := os.Args[1]
+	etcd, err := NewEtcd(endpoint)
+
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	sigch := make(chan os.Signal, 1)
+	signal.Notify(sigch, os.Interrupt)
+
+	go func() {
+		<-sigch
+		os.Exit(0)
+	}()
+
+	etcd.Watch("/paus/users/", callback)
 }
