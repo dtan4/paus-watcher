d1771ff492bdadf0d2994117e1cdd8ec7aa7fe7e
Daisuke Fujita <dtanshi45@gmail.com>
2016-07-04 10:16:11 +0900

    Move to store/etcd

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/coreos/etcd/client"
 	"github.com/dtan4/paus-watcher/config"
+	"github.com/dtan4/paus-watcher/store"
 )
 
 const (
@@ -25,7 +26,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	etcd, err := NewEtcd(config.EtcdEndpoint)
+	etcd, err := store.NewEtcd(config.EtcdEndpoint)
 
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
